09b85042d7ed5ecb24fd1124923d2e71c91942a1
VINOTH KUMAR A <vinothkumarsty@gmail.com>
2025-09-03 13:25:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function start()
+{
+    var side = document.getElementById("side")
+    side.style.display = "flex"
+}
+function end1()
+{
+    var side = document.getElementById("side")
+    side.style.display = "none"
+    
+}
